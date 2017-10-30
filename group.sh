46b2e57a0eb7ff96f20aee7e328115076c556aec
Steven Lin <stevenlin@Stevens-MacBook-Pro.local>
2017-10-30 13:35:33 +0000

    add out option

--- group.sh
+++ group.sh
@@ -2,4 +2,4 @@
 #$ -N test-cluster
 #$ -q short.qc
 # clustering the samples by genetic similarity
-/users/bag/clme1992/bin/plink --bfile /data/jaga/stophcv/steven/August2016/step2/August2016 --cluster --cc --ppc 0.01
+/users/bag/clme1992/bin/plink --bfile /data/jaga/stophcv/steven/August2016/step2/August2016 --cluster --cc --ppc 0.01 --out version2
