6ed9a1ef9548c46aeef06124f0271defc23481f0
Steven Lin <clme1992@jaga.(none)>
2017-11-28 11:25:17 +0000

    hello

--- get_pheno.py
+++ get_pheno.py
@@ -1,8 +1,9 @@
 import re
-pheno_file_name1 = "/data/jaga/stophcv/steven/clinical_data/TR000384_ID"
-pheno_file_name2 = "/data/jaga/stophcv/steven/clinical_data/TR000402_noCirrhosis_ID"
-file_name = "/data/jaga/stophcv/steven/August2016/test/August2016.fam"
-wrt_file_name = "August2016.fam.temp"
+prefix = "/data/jaga/stophcv/steven/August2016/"
+pheno_file_name1 = prefix + "clinical_data/TR000384_ID"
+pheno_file_name2 = prefix + "clinical_data/TR000402_noCirrhosis_ID"
+file_name = prefix + "test/August2016.fam"
+wrt_file_name = prefix + "test/August2016.fam.temp"
 wrt_file = open(wrt_file_name, 'w')
 case_ID = list()
 control_ID = list()
@@ -17,19 +18,19 @@ with open(pheno_file_name2, 'r') as read_file:
 
 with open (file_name, 'r') as read_file:
     for line in read_file:
-        line = line.rstrip().split(' ')[0]
-        line_lst = re.split('[-_]', line)
+        line = line.rstrip().split(' ')
+        line_lst = re.split('[-_]', line[0])
         ID = ""
         for item in line_lst:
             if len(item) == 10:
                 ID = item
                 break
         if ID in case_ID:
-            line[5] = 2
+            line[5] = '2'
         elif ID in control_ID:
-            line[5] = 1
+            line[5] = '1'
         else:
-            line[5] = 0
+            line[5] = '-9'
         print(' '.join(line), file=wrt_file)
         
 
