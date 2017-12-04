de42c67684b1441f92e57f6cce1f3e180d72099d
Steven Lin <stevenlin@Stevens-MacBook-Pro.local>
2017-12-04 13:30:36 +0000

    covar

--- build_covar.py
+++ build_covar.py
@@ -1,5 +1,13 @@
 #get the covariates of age
 import re
+import sys
+import statistics
+impute = 0
+if len(sys.argv) > 1 and sys.argv[1] == 'impute':
+    impute = 1
+
+med = None
+age_lst = list()
 prefix = "/data/jaga/stophcv/steven/August2016/"
 covar_dict = dict()
 file_name = prefix + "clinical_data/patient_lib.tsv"
@@ -13,9 +21,16 @@ with open(file_name, 'r') as read_file:
             age = None
         else:
             age = 2017 - age
+            age_lst.append(age)
         covar_dict[ID] = age
+if impute == 1:
+    med = statistics.median(age_lst)
 
-wrt_file_name = prefix + "test/August2016.covar"
+wrt_file_name = ""
+if impute == 1:
+    wrt_file_name = prefix + "test/August2016.covar.impute"
+else:
+    wrt_file_name = prefix + "test/August2016.covar"
 wrt_file = open(wrt_file_name, 'w')
 file_name = prefix + "test/August2016.fam"
 missing_num = 0
@@ -31,7 +46,10 @@ with open(file_name, 'r') as read_file:
         if (ID in covar_dict) and (covar_dict[ID] != None):
             string = '1' + '\t' + str(covar_dict[ID])
         else:
-            string = '1' + '\t'
             missing_num = missing_num + 1
+            if (impute == 1):
+                string = '1' + '\t' + str(med)
+            else:
+                string = '1' + '\t'
         print(string, file=wrt_file)
         print(missing_num)
