4279afa818baab8d12c83ccb7de908396941abda
Steven Lin <stevenlin@Stevens-MacBook-Pro.local>
2017-12-11 11:14:07 +0000

    hi

--- check_uniq.py
+++ check_uniq.py
@@ -24,12 +24,15 @@ with open(file_name,'r', encoding='latin-1') as read_file:
                 gene_ID_lst.append(item)
 
 uniq_cnt_lst = list()
+not_prim_cnt = 0
 for prim_ID in ID_dict:
     ID_lst = ID_dict[prim_ID]
     uniq_cnt = -1
-    for ID in ID_lst:
+    for i,ID in enumerate(ID_lst):
         if ID in gene_ID_lst:
             uniq_cnt = uniq_cnt + 1
+        if i != 0:
+            not_prim_cnt = not_prim_cnt + 1
     uniq_cnt_lst.append(uniq_cnt)
 
 for item in uniq_cnt_lst:
